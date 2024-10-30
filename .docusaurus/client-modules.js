cca23f03ada5fe4690cfb8130641035976380964
Fouly (Ahmed Elfouly) <ahmed.elfouly@booking.com>
2024-10-30 22:10:00 +0100

    Initial docusaurus commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/aelfouly/The_Quarter/open-atlas/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/aelfouly/The_Quarter/open-atlas/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/aelfouly/The_Quarter/open-atlas/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/aelfouly/The_Quarter/open-atlas/src/css/custom.css"),
+];
