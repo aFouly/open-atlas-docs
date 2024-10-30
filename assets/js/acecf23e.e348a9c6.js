b0f1f228bd3d7bda84ba8e55c0766d48b253e700
Fouly (Ahmed Elfouly) <ahmed.elfouly@booking.com>
2024-10-30 22:32:05 +0100

    Deploy website - based on cca23f03ada5fe4690cfb8130641035976380964

---
+++ assets/js/acecf23e.e348a9c6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopen_atlas=self.webpackChunkopen_atlas||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
